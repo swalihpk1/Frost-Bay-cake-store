d04198956cb007a1bbae98cae3be70aec9f1961f
Swalih pk <nisunasih135@gmail.com>
2023-12-29 09:58:13 +0530

    Price sorting done(lowToHigh,highTolow,popular)

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,13 +6,13 @@ const session = require("express-session");
 
 const shop = async (req, res) => {
     try {
-
         console.log(req.query);
         const userId = req.userId;
         const user = await User.findOne({ _id: userId });
         const currentPage = parseInt(req.query.page) || 1;
         const searchTerm = req.query.q;
         const selectedCategories = req.query.category || [];
+        const sortOption = req.query.sort || 'popularity'; // Default sorting option
 
         let query = {};
 
@@ -29,14 +29,23 @@ const shop = async (req, res) => {
             query.category = { $in: selectedCategories };
         }
 
+        // Add sorting logic based on the selected sort option
+        let sortCriteria = {};
+        if (sortOption === 'lowToHigh') {
+            sortCriteria = { price: 1 }; // Sort low to high by price
+        } else if (sortOption === 'highToLow') {
+            sortCriteria = { price: -1 }; // Sort high to low by price
+        }
+
         const totalProducts = await Products.countDocuments(query);
         const totalPages = Math.ceil(totalProducts / 6);
 
         const products = await Products.find(query)
             .populate({ path: 'category', model: 'Categorys', select: 'categoryName' })
+            .sort(sortCriteria) // Apply sorting
             .skip((currentPage - 1) * 6)
             .limit(6);
-        
+
         const category = await Category.find({});
         const userDetails = await User.populate(user, { path: 'cart.productId', model: 'products' });
 
@@ -51,21 +60,17 @@ const shop = async (req, res) => {
             selectedCategories: selectedCategories,
         };
 
-        // console.log("nvn "+response.selectedCategories);
-
-        // checking the client accept json resposes
+        // checking the client accept JSON responses
         if (req.xhr || req.headers.accept.indexOf('json') > -1) {
             return res.json(response);
         }
-        
-        res.render('shop', response);
 
+        res.render('shop', response);
     } catch (error) {
         console.log(error.message);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
-
 
 
 
@@ -81,7 +86,6 @@ const productDetails = async (req, res) => {
         console.log(error.message);
     }
 }
-
 
 
 module.exports = {
